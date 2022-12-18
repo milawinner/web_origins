e734a4ac78037cbd61f8a484ef8fe7f657980782
Mila <lakralima@mail.ru>
2022-12-18 18:36:55 +0300

    Base with corrections

--- hometaskphp/my.sql
+++ hometaskphp/my.sql
@@ -1,23 +1,23 @@
 
 -- create
-CREATE TABLE CLASSMATES (
+CREATE TABLE classmates (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  age TEXT NOT NULL,
+  age INTEGER NOT NULL,
   address TEXT NOT NULL
 );
 
 -- insert
-INSERT INTO CLASSMATES VALUES (01, 'Арсений', '19', 'Москва');
-INSERT INTO CLASSMATES VALUES (02, 'Артемий', '39', 'Подмосковье');
-INSERT INTO CLASSMATES VALUES (03, 'Марисий', '28', 'Ужгород');
-INSERT INTO CLASSMATES VALUES (04, 'Мариса', '33', 'Санкт-Петербург');
-INSERT INTO CLASSMATES VALUES (05, 'Маркиза', '21', 'Москва');
-INSERT INTO CLASSMATES VALUES (06, 'Парис', '55', 'Париж');
-INSERT INTO CLASSMATES VALUES (07, 'Патрис', '66', 'Пермь');
-INSERT INTO CLASSMATES VALUES (08, 'Патрисия', '34', 'Москва');
-INSERT INTO CLASSMATES VALUES (09, 'Карл', '90', 'Лондон');
-INSERT INTO CLASSMATES VALUES (10, 'Карла', '23', 'Баден-Баден');
+INSERT INTO classmates VALUES (01, 'Арсений', 19, 'Москва');
+INSERT INTO classmates VALUES (02, 'Артемий', 39, 'Подмосковье');
+INSERT INTO classmates VALUES (03, 'Марисий', 28, 'Ужгород');
+INSERT INTO classmates VALUES (04, 'Мариса', 33, 'Санкт-Петербург');
+INSERT INTO classmates VALUES (05, 'Маркиза', 21, 'Москва');
+INSERT INTO classmates VALUES (06, 'Парис', 55, 'Париж');
+INSERT INTO classmates VALUES (07, 'Патрис', 66, 'Пермь');
+INSERT INTO classmates VALUES (08, 'Патрисия', 34, 'Москва');
+INSERT INTO classmates VALUES (09, 'Карл', 90, 'Лондон');
+INSERT INTO classmates VALUES (10, 'Карла', 23, 'Баден-Баден');
 
 -- fetch 
-SELECT name FROM CLASSMATES WHERE address = 'Москва' and age >= 18 and age < 30;
+SELECT name FROM classmates WHERE address = 'Москва' AND age >= 18 AND age < 30;
